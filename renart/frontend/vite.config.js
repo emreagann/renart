26bc0ea2158ed8633b5f0215fb0b4f8d82283e51
emreagann <159646253+emreagann@users.noreply.github.com>
2025-10-06 10:03:37 +0300

    Update vite.config.js

--- renart/frontend/vite.config.js
+++ renart/frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: './frontend',
   plugins: [react()],
-  root: 'frontend',
   base:process.env.VITE_BASE_PATH || "/react-vite-deploy",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend/src'),
+    },
+  },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
-
-
