0a3b15e89853a8223faa1cae0e37ee888c414286
emreagann <159646253+emreagann@users.noreply.github.com>
2025-10-06 10:04:25 +0300

    Update main.jsx

--- renart/frontend/src/main.jsx
+++ renart/frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
-
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
